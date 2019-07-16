5c4a3dbde8a0918b8af222ea4af3e1e557fa8930
swede1987 <40723088+swede1987@users.noreply.github.com>
2019-07-16 11:04:06 +0500

    Creat auth.php

---
+++ auth.php
@@ -0,0 +1,7 @@
+<?php
+require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php');
+global $USER;
+$USER_ID = 1; //зачастую именно он админ
+$USER->Authorize($USER_ID); // авторизуем
+require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php');
+?>
